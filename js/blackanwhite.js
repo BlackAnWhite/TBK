fe3e65b1b80124e1466ade38b797f4a4041d79f8
BlackAnWhite <284549569@qq.com>
2017-12-09 10:21:26 +0800

    v 0.0.2

--- js/blackanwhite.js
+++ js/blackanwhite.js
@@ -3,3 +3,31 @@
  * author:BlackAnWhite;
  * 2017-12-07
  */
+
+$(function() {
+	var clientWidth = document.documentElement.clientWidth;
+	var fontSize = parseFloat(document.documentElement.style.fontSize);
+	$('.setWH').each(function() {
+		if($(this).data('width')) {
+			$(this).css({
+				width: $(this).data('width') * fontSize
+			});
+		}
+		if($(this).data('height')) {
+			$(this).css({
+				height: $(this).data('height') * fontSize
+			});
+		}
+		if($(this).data('lineHeight')) {
+			$(this).css({
+				lineHeight: $(this).data('lineHeight') * fontSize
+			});
+		}
+	});
+});
+
+$(function() {
+	$('.autoimg').each(function(index, item) {
+		$(this).css("background-image", 'url(' + $(this).data('src') + ')');
+	});
+});
