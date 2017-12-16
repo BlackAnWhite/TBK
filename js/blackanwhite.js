aee20b185aeb05b9e8bfcd79aecedf8c33dcc14f
BlackAnWhite <284549569@qq.com>
2017-12-16 17:45:13 +0800

    v 171216

--- js/blackanwhite.js
+++ js/blackanwhite.js
@@ -26,7 +26,7 @@ function getFontSize(_client) {
 }
 getFontSize(750);
 
-//解决2k屏幕的宽高不适配rem问题
+//解决垃圾QQ浏览器2k屏幕下的宽高不适配rem问题
 $(function() {
 	var clientWidth = document.documentElement.clientWidth;
 	var fontSize = parseFloat(document.documentElement.style.fontSize);
@@ -55,3 +55,83 @@ $(function() {
 		$(this).css("background-image", 'url(' + $(this).data('src') + ')');
 	});
 });
+//进度条君
+$(function(){
+	$('.pmgressBarBox').each(function(){
+		var nowNum = parseFloat($(this).find('.nowNum').text()),
+		 allNum = parseFloat($(this).find('.allNum').text()),
+		 ratio = nowNum/allNum*100 + '%';
+		 $(this).find('.now').css({width:ratio})
+	});
+});
+//倒计时
+$(function(){
+	var date = $('#run').data('time'); //获取时间字符串
+	if(date){
+		lastTime(); 
+	}
+	function lastTime(){
+			date = new Date(Date.parse(date.replace(/-/g, "/")));  //转换成 Thu May 08 2014 00:22:11 GMT+0800 (中国标准时间)
+	date = date.getTime(); //转换成时间戳	
+	var now =date - new Date().getTime();  //获取当前时间差
+	
+	var oBox = $('#run').find('span'); //获取倒计时容器
+		
+	var last = setInterval(function(){
+	  var	times = [] ;
+		var hour = Math.floor(now/1000/60/60),
+			min = Math.floor(now/60000 - hour * 60),
+			sec = Math.floor(now/1000 - hour*60*60 - min*60);
+//			console.log(hour,sec,min);
+		times.push( hour >= 10 ? hour : '0' + hour );
+		times.push( min >= 10 ? min : '0' + min );
+		times.push( sec >= 10 ? sec : '0' + sec );
+		oBox.each(function(index){
+			$(this).html(times[index]);
+		});
+		now -= 1000;
+		if (now <= 0) {
+			now = 0 ;
+			clearInterval(last) ;
+			};
+	},1000);
+	}
+
+});
+
+//点击查看图文详情
+$(function(){
+	$('.picInfoTit').click(function(){
+		$('.picInfoBox').slideToggle();
+	});
+});
+//一键复制到剪贴板
+$(function() {
+//	var str = $("#copy_msg_one").html();
+//	$("#copy_msg_one").val(str);
+	copyUrl($("#copy_url_one"));
+	function copyUrl(dom) {
+		dom.click(function() {
+			var url = $("#copy_msg_one") //根据实际情况更改,需要复制内容的载体
+			url.select();
+			document.execCommand("Copy");
+//			alert("已复制至剪切板");
+			$('.alertBox , .blackBg').hide();			
+			showSucc();			
+		})
+	};
+	$('.botBtn').click(function(){
+		$('.blackBg , .alertBox').show();
+	});
+	$('.blackBg').click(function(){
+		$('.alertBox , .blackBg').hide();
+	});
+	function showSucc(){
+		$('.succBox').show(500,'linear',function(){
+			setTimeout(function(){
+				$('.succBox').hide(500);
+			},1000)
+			
+		});
+	};
+})
